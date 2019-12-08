0d00389c35d0e1a718c19c740ce87c70440b87c7
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-08 20:56:40 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+require 'pry'
+class Anagram
+  
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(matched)
+    matched.select do |matched|
+      @word.split("").sort == matched.split("").sort
+    end
+  end
+end
