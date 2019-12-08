4ac9f3daf40af7d69d839090080bd31ffad55a67
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-08 20:56:05 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,15 +1,16 @@
 require 'pry'
-class Anagram 
- attr_accessor :word 
- 
- def initialize(word)
-   @word = word 
- end 
- 
- # based on anagram matches will provide an array of words. 
- def match(matched)
-   matched.select.do|matched|
-     @word.split("").sort == matched.split("").sort
-   end 
-  end 
+class Anagram
+  
+  attr_accessor :word
 
+  def initialize(word)
+    @word = word
+  end
+
+  # find anagram matches given an array of words
+  def match(matched)
+    matched.select do |matched|
+      @word.split("").sort == matched.split("").sort
+    end
+  end
+end
