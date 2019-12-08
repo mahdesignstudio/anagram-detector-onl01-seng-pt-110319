204a0e6110e9abbad37232a306240a5db2e79de8
Melissa Ramirez <melissa.ramirez08@yahoo.com>
2019-12-08 20:50:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ class Anagram
  # based on anagram matches will provide an array of words. 
  def match(matching_word)
    matching_word.select.do |matching_word|
-     @word.split("").sort == matching_word.split("").sort 
- 
-  
+     @word.split("").sort == matching_word.split("").sort
+   end 
+  end 
 end 
